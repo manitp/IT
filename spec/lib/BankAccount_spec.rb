721456d314bf0e05d66dd46718a973b396035bcc
manitp <manitpadungkit@gmail.com>
2014-10-21 20:44:47 +0700

    2

--- spec/lib/BankAccount_spec.rb
+++ spec/lib/BankAccount_spec.rb
@@ -4,9 +4,9 @@ require "BankAccount"
 describe BankAccount do
   describe "#check_balance" do
     it "check the deposit" do
-   	  account1=BankAccount.new(6000)
+   	  account1=BankAccount.new(5000)
       balance=account1.deposit(40000)
-      expect(balance).to eq(46000)
+      expect(balance).to eq(45000)
     end
     it "check the withdraw" do
    	  account1=BankAccount.new(5000)

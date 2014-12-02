101d8674cf8a3aad2093fe7e5c203e0c8040eeb1
manitp <manitpadungkit@gmail.com>
2014-12-02 15:46:03 +0700

    4

--- lib/lovemac.rb
+++ lib/lovemac.rb
@@ -2,55 +2,55 @@ def lovemac(number)
   # Put your code in here  
   @word = "";
   @num = 0;
-if (number == 1) {puts 1; @num = 1; return @num;};end
-if (number == 2) {puts 2; @num = 2; return @num;};end
+if (number == 1) puts "1"; @num = 1; return @num;};end
+if (number == 2) {puts "2"; @num = 2; return @num;};end
 if (number ==3) {puts "Love";@word = "Love";return @word};end
-if (number == 4) {puts 4; @num = 4; return @num;};end
+if (number == 4) {puts "4"; @num = 4; return @num;};end
 if (number == 5) {puts "Mac";@word ="Mac";return @word};end
 if (number == 6) {puts "Love";@word = "Love";return @word};end
-if (number == 7) {puts 7; @num = 7; return @num;};end
-if (number == 8) {puts 8; @num = 8; return @num;};end
+if (number == 7) {puts "7"; @num = 7; return @num;};end
+if (number == 8) {puts "8"; @num = 8; return @num;};end
 if (number == 9) {puts "Love";@word = "Love";return @word};end
 if (number == 10) {puts "Mac";@word ="Mac";return @word};end
-if (number == 11) {puts 11; @num = 11; return @num;};end
+if (number == 11) {puts "11"; @num = 11; return @num;};end
 if (number == 12) {puts "Love";@word = "Love";return @word};end
-if (number == 13) {puts 13; @num = 13; return @num;};end
-if (number == 14) {puts 14; @num = 14; return @num;};end
+if (number == 13) {puts "13"; @num = 13; return @num;};end
+if (number == 14) {puts "14"; @num = 14; return @num;};end
 if (number == 15) {puts "HateWindows";@word="HateWindows";return @word};end
-if (number == 16) {puts 16; @num = 16; return @num;};end
-if (number == 17) {puts 17; @num = 17; return @num;};end
+if (number == 16) {puts "16"; @num = 16; return @num;};end
+if (number == 17) {puts "17"; @num = 17; return @num;};end
 if (number == 18) {puts "Love";@word = "Love";return @word};end
-if (number == 19) {puts 19; @num = 19; return @num;};end
+if (number == 19) {puts "19"; @num = 19; return @num;};end
 if (number == 20) {puts "Mac";@word ="Mac";return @word};end
 if (number == 21) {puts "Love";@word = "Love";return @word};end
-if (number == 22) {puts 22; @num = 22; return @num;};end
-if (number == 23) {puts 23; @num = 23; return @num;};end
+if (number == 22) {puts "22"; @num = 22; return @num;};end
+if (number == 23) {puts "23"; @num = 23; return @num;};end
 if (number == 24) {puts "Love";@word = "Love";return @word};end
 if (number == 25) {puts "Mac";@word ="Mac";return @word};end
-if (number == 26) {puts 26; @num = 26; return @num;};end
+if (number == 26) {puts "26"; @num = 26; return @num;};end
 if (number == 27) {puts "Love";@word = "Love";return @word};};end
-if (number == 28) {puts 28; @num = 28; return @num;};end
-if (number == 29) {puts 29; @num = 29; return @num;};end
+if (number == 28) {puts "28"; @num = 28; return @num;};end
+if (number == 29) {puts "29"; @num = 29; return @num;};end
 if (number == 30) {puts "HateWindows";@word="HateWindows";return @word};end
-if (number == 31) {puts 31; @num = 31; return @num;};end
-if (number == 32) {puts 32; @num = 32; return @num;};end
+if (number == 31) {puts "31"; @num = 31; return @num;};end
+if (number == 32) {puts "32"; @num = 32; return @num;};end
 if (number == 33) {puts "Love";@word = "Love";return @word};end
-if (number == 34) {puts 34; @num = 34; return @num;};end
+if (number == 34) {puts "34"; @num = 34; return @num;};end
 if (number == 35) {puts "Mac";@word ="Mac";return @word};end
 if (number == 36) {puts "Love";@word = "Love";return @word};end
-if (number == 37) {puts 37; @num = 37; return @num;};end
-if (number == 38) {puts 38; @num = 38; return @num;};end
+if (number == 37) {puts "37"; @num = 37; return @num;};end
+if (number == 38) {puts "38"; @num = 38; return @num;};end
 if (number == 39) {puts "Love";@word = "Love";return @word};end
 if (number ==40) {puts "Mac";@word ="Mac";return @word};end
-if (number == 41) {puts 41; @num = 41; return @num;1};end
+if (number == 41) {puts "41"; @num = 41; return @num;1};end
 if (number ==42) {puts "Love";@word = "Love";return @word};end
-if (number == 43) {puts 43; @num = 43; return @num;};end
-if (number == 44) {puts 44; @num = 44; return @num;};end
+if (number == 43) {puts "43"; @num = 43; return @num;};end
+if (number == 44) {puts "44"; @num = 44; return @num;};end
 if (number == 45) {puts "HateWindows";@word="HateWindows";return @word};end
-if (number == 46) {puts 46; @num = 46; return @num;};end
-if (number == 47) {puts 47; @num = 47; return @num;};end
+if (number == 46) {puts "46"; @num = 46; return @num;};end
+if (number == 47) {puts "47"; @num = 47; return @num;};end
 if (number == 48) {puts "Love";@word = "Love";return @word};end
-if (number == 49) {puts 49; @num = 49; return @num;};end
+if (number == 49) {puts "49"; @num = 49; return @num;};end
 if (number == 50) {puts "Mac";@word ="Mac";return @word};end
 
   	
